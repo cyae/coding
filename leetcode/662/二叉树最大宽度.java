190b1b3d2faccf6aeff6c3778e0cab5ed5c3c5c3
7up <cyae@users.noreply.github.com>
2023-04-28 21:25:03 +0800

    add dependency

--- leetcode/662/二叉树最大宽度.java
+++ leetcode/662/二叉树最大宽度.java
@@ -5,19 +5,25 @@
  */
 
 // @lc code=start
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import dependency.TreeNode;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
